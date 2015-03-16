80df2e161a012aa3def64c81aa59da502c54ba41
Jan Gabriel <jan.gabriel@tul.cz>
2015-03-16 09:29:03 +0100

    added scroll easing

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-var frak = function(){
-'use strict'
-$(document).ready(function() {
-    $('#Carousel').carousel({
-        interval: 5000
+var frak = function() {
+    'use strict'
+    $(document).ready(function() {
+        $('#Carousel').carousel({
+            interval: 5000
+        });
     });
-});
 
- $(window).load(function() {
-    $(document.body).fadeIn(1000);
- });
+    $(window).load(function() {
+        $(document.body).fadeIn(1000);
+    });
+
+    $('nav ul li a').each(function() {
+        $(this).click(function(e) {
+            $("html, body").animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 800);
+            e.preventDefault();
+        });
+    });
 }
 frak();
