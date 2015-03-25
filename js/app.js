67f534072004d0871ae3cf81ec37fa0f06cddbb3
Jan Gabriel <jan.gabriel@tul.cz>
2015-03-25 09:04:22 +0100

    fixed scroll

--- js/app.js
+++ js/app.js
@@ -12,10 +12,13 @@ var frak = function() {
 
     $('nav ul li a').each(function() {
         $(this).click(function(e) {
-            $("html, body").animate({
-                scrollTop: $($(this).attr('href')).offset().top
-            }, 800);
             e.preventDefault();
+            var target = this.hash;
+            $("html, body").animate({
+                scrollTop: $(this.hash).offset().top
+            }, 800, function() {
+                    return window.history.pushState(null, null, target);
+                });
         });
     });
 }
